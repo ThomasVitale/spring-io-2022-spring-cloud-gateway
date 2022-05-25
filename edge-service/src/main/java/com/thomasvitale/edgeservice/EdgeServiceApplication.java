f589e24507f73ac8e071f47aa8b4734d36866800
Thomas Vitale <ThomasVitale@users.noreply.github.com>
2022-05-25 16:11:46 +0200

    Configure rater limiter

--- edge-service/src/main/java/com/thomasvitale/edgeservice/EdgeServiceApplication.java
+++ edge-service/src/main/java/com/thomasvitale/edgeservice/EdgeServiceApplication.java
@@ -2,12 +2,21 @@ package com.thomasvitale.edgeservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.gateway.filter.ratelimit.KeyResolver;
+import org.springframework.context.annotation.Bean;
+
+import reactor.core.publisher.Mono;
 
 @SpringBootApplication
 public class EdgeServiceApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(EdgeServiceApplication.class, args);
+    }
+
+    @Bean
+    KeyResolver keyResolver() {
+        return exchange -> Mono.just("ANONYMOUS");
     }
 
 }
