0ea8dc06cbaa583c6a70198e2f014ad79cbb3ddc
Thomas Vitale <ThomasVitale@users.noreply.github.com>
2022-05-25 15:22:19 +0200

    Initial commit

---
+++ book-service/src/main/java/com/thomasvitale/bookservice/BookServiceApplication.java
@@ -0,0 +1,37 @@
+package com.thomasvitale.bookservice;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Flux;
+
+@SpringBootApplication
+public class BookServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BookServiceApplication.class, args);
+    }
+
+}
+
+record Book(String title) {}
+
+@RestController
+class BookController {
+
+    private static final Logger log = LoggerFactory.getLogger(BookController.class);
+
+    @GetMapping("books")
+    public Flux<Book> getBooks() {
+        return Flux.just(
+                new Book("Harry Potter"),
+                new Book("His Dark Materials"),
+                new Book("The Hobbit"),
+                new Book("The Lord of the Rings")
+        ).doFirst(() -> log.info("Returning list of books in the catalog"));
+    }
+
+}
---
+++ edge-service/Dockerfile
@@ -0,0 +1,15 @@
+FROM eclipse-temurin:17 AS builder
+WORKDIR workspace
+ARG JAR_FILE=build/libs/*.jar
+COPY ${JAR_FILE} edge-service.jar
+RUN java -Djarmode=layertools -jar edge-service.jar extract
+
+FROM eclipse-temurin:17
+RUN useradd spring
+USER spring
+WORKDIR workspace
+COPY --from=builder workspace/dependencies/ ./
+COPY --from=builder workspace/spring-boot-loader/ ./
+COPY --from=builder workspace/snapshot-dependencies/ ./
+COPY --from=builder workspace/application/ ./
+ENTRYPOINT ["java", "org.springframework.boot.loader.JarLauncher"]
