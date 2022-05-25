ba8ffca57c7f0e7f2b870ae99e4ca52e602a9a25
Thomas Vitale <ThomasVitale@users.noreply.github.com>
2022-05-25 16:25:01 +0200

    Configure circuit breaker

--- edge-service/src/main/java/com/thomasvitale/edgeservice/EdgeServiceApplication.java
+++ edge-service/src/main/java/com/thomasvitale/edgeservice/EdgeServiceApplication.java
@@ -4,7 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.gateway.filter.ratelimit.KeyResolver;
 import org.springframework.context.annotation.Bean;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
 @SpringBootApplication
@@ -19,4 +22,14 @@ public class EdgeServiceApplication {
         return exchange -> Mono.just("ANONYMOUS");
     }
 
+}
+
+@RestController
+class FallbackController {
+
+    @GetMapping("/books-fallback")
+    Flux<Void> getBooksFallback() {
+        return Flux.empty();
+    }
+    
 }
